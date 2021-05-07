d25d9049006ac0ec31767c408905eb2725777f71
Usarneme <jamestcoop@gmail.com>
2021-05-07 15:52:27 -0700

    :lipstick: adds styling and functionality to the UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -23,10 +23,10 @@ namespace ProgramNamespace
 
     public static void ShowMenu()
     {
-      Console.WriteLine("\tMenu\t\tPrice\t\t\tToday's Specials");
-      Console.WriteLine("\t----\t\t-----\t\t\t----------------");
-      Console.WriteLine("\tLoaf of Bread\t$5\t\t\tBuy Two (2) Loaves And Get One (1) Free!");
-      Console.WriteLine("\tHot Pastry\t$2\t\t\tThree (3) Pastries For $5!\n\n");
+      Console.WriteLine("\tMenu\t\t\tPrice\t\t\tToday's Specials");
+      Console.WriteLine("\t----\t\t\t-----\t\t\t----------------");
+      Console.WriteLine("\tLoaf of Bread\t\t$5\t\t\tBuy Two (2) Loaves And Get One (1) Free!");
+      Console.WriteLine("\tDelicious Pastry\t$2\t\t\tThree (3) Pastries For $5!\n\n");
     }
 
     public static void ShowOptions()
@@ -54,20 +54,21 @@ namespace ProgramNamespace
           Console.WriteLine("\n\tYou have ordered:");
           if (numberOfBreads == 1)
           {
-            Console.Write("\t{0} loaf of bread. ");
+            Console.Write("\t{0} loaf of bread. ", numberOfBreads);
           }
           else
           {
-            Console.Write("\t{0} loaves of bread. ");
+            Console.Write("\t{0} loaves of bread. ", numberOfBreads);
           }
           if (numberOfPastries == 1)
           {
-            Console.Write("\t{0} delicious pastry. ");
+            Console.Write("\n\t{0} delicious pastry. ", numberOfPastries);
           }
           else
           {
-            Console.Write("\t{0} delicious pastries. ");
+            Console.Write("\n\t{0} delicious pastries. ", numberOfPastries);
           }
+          Console.WriteLine("\n");
         }
         else
         {
@@ -80,6 +81,19 @@ namespace ProgramNamespace
         int orderCost = PierresBakery.GetCurrentOrderCost();
         Console.WriteLine("\n\tYour total comes to ${0}.00.\n", orderCost);
         OrderUI();
+      }
+      else if (command.ToLower() == "b")
+      {
+        BreadOrderUI();
+      }
+      else if (command.ToLower() == "p")
+      {
+        PastryOrderUI();
+      }
+      else if (command.ToLower() == "c")
+      {
+        CheckoutUI();
+        _isOrdering = false;
       }
       else if (command.ToLower() == "e")
       {
@@ -95,9 +109,72 @@ namespace ProgramNamespace
 
     public static void BreadOrderUI()
     {
-      ShowOptions();
-      Console.WriteLine("\n\tHow many loaves of bread would you like today?\n\t: ");
-      int numberOfLoaves = Console.Read();
+      Console.Write("\n\tHow many loaves of bread would you like today?\n\t: ");
+      string loafString = Console.ReadLine();
+      int numberOfLoaves = int.Parse(loafString);
+      if (numberOfLoaves == 0)
+      {
+        Console.WriteLine("\n\tOk, no bread. Returning to main menu.\n");
+        OrderUI();
+      }
+      else
+      {
+        PierresBakery.AddToCurrentOrder("Breads", numberOfLoaves);
+        if (numberOfLoaves == 1)
+        {
+          Console.WriteLine("\n\tOne loaf of bread added to your order. Returning to the main menu.\n");
+          OrderUI();
+        }
+        else
+        {
+          Console.WriteLine("\n\t{0} loaves of bread added to your order. Returning to the main menu.\n", numberOfLoaves);
+          OrderUI();
+        }
+      }
+    }
+
+    public static void PastryOrderUI()
+    {
+      Console.Write("\n\tHow many delicious pastries would you like today?\n\t: ");
+      string pastriesString = Console.ReadLine();
+      int numberOfPastries = int.Parse(pastriesString);
+      if (numberOfPastries == 0)
+      {
+        Console.WriteLine("\n\tOk, no delicious pastries. Returning to main menu.\n");
+        OrderUI();
+      }
+      else
+      {
+        PierresBakery.AddToCurrentOrder("Pastries", numberOfPastries);
+        if (numberOfPastries == 1)
+        {
+          Console.WriteLine("\n\tOne delicious pastry added to your order. Returning to the main menu.\n");
+          OrderUI();
+        }
+        else
+        {
+          Console.WriteLine("\n\t{0} delicious pastries added to your order. Returning to the main menu.\n", numberOfPastries);
+          OrderUI();
+        }
+      }
+    }
+
+    public static void CheckoutUI()
+    {
+      Dictionary<string, int> itemsOrdered = PierresBakery.GetCurrentOrderItems();
+      int numberOfBreads = itemsOrdered["Breads"];
+      int numberOfPastries = itemsOrdered["Pastries"];
+      if (numberOfBreads > 0 || numberOfPastries > 0)
+      {
+        int orderCost = PierresBakery.GetCurrentOrderCost();
+        Console.WriteLine("\n\tThanks for visiting us today! ");
+        Console.WriteLine("\tYour total comes to ${0}.00.", orderCost);
+        Console.WriteLine("\n\tPlease enter your credit card number and we'll process your payment.\n\n ;)");
+      }
+      else
+      {
+        Console.WriteLine("\n\n\tOh no! You forgot to order anything. Maybe next time... Have a good day!\n\n");
+      }
     }
   }
 }
